88368ad5c7f987f71eac1d906a6bb31fdcbf4c85
Muhammad Rifqi <muhammad45rifki@gmail.com>
2022-08-23 13:26:38 +0700

    fifth

--- service-worker.js
+++ service-worker.js
@@ -6,3 +6,7 @@ self.addEventListener('install', (event)=>{
 self.addEventListener('activate', (event)=>{
 	console.log('Update Activated',event);
 })
+
+self.addEventListener('fetch', function (event) {
+    console.log('Update Fetched',event);
+});
